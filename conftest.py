6379fb1eb36ddd4e86d8a998efdc02e610d0a193
Uliana Krekhovetska <uliana.krehovetska@gmail.com>
2023-02-16 19:23:01 +0200

    API tests

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from modules.api.clients.github import GitHub
 
 
 class User:
@@ -22,3 +23,9 @@ def user():
     yield user
 
     user.remove()
+
+@pytest.fixture
+def github_api():
+    api = GitHub()
+
+    yield api
